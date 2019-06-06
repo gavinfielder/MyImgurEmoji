2a52f42563ebce0d8eecd652056b6e3862249683
Gavin Fielder <gfielder@42.us.org>
2019-06-06 14:12:53 -0700

    can detect every change in a textarea, after clicking the browser action button

--- content.js
+++ content.js
@@ -5,37 +5,36 @@ chrome.runtime.onMessage.addListener(
 	{
 		if (request.message === "clicked_browser_action")
 		{
-			var firstHref = $("a[href^='http']").eq(0).attr("href");
-			console.log(firstHref);
-			chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
+			var elements = document.querySelectorAll('textarea');
+			for (var i = 0; i < elements.length; i++)
+			{
+				var element = elements[i];
+				console.log("Got element:" + element + " with classname: \"" + element.className + "\"");
+				element.addEventListener("input", 
+					function(eventObject)
+					{
+						console.log("text area changed!");
+					}
+				);
+			}
+			
+			var elements = document.querySelectorAll('input');
+			for (var i = 0; i < elements.length; i++)
+			{
+				var element = elements[i];
+				console.log("Got input element:" + element);
+				element.addEventListener("click", 
+					function(eventObject)
+					{
+						console.log("input clicked!");
+					}
+				);
+			}
+
+
 		}
 	}
 );
 
-var elements = document.querySelectorAll('textarea');
-for (var i = 0; i < elements.length; i++)
-{
-	var element = elements[i];
-	console.log("Got element:" + element + " with classname: \"" + element.className + "\"");
-	element.addEventListener("click", 
-		function(eventObject)
-		{
-			console.log("text area changed!");
-		}
-	);
-}
-
-var elements = document.querySelectorAll('input');
-for (var i = 0; i < elements.length; i++)
-{
-	var element = elements[i];
-	console.log("Got input element:" + element);
-	element.addEventListener("click", 
-		function(eventObject)
-		{
-			console.log("input clicked!");
-		}
-	);
-}
 
 console.log("gavin's content script is done now.");
